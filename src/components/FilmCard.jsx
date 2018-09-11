dedb21f74072e55fde16f352c9bd65fc67360340
lbarret4 <llewellynbar4@gmail.com>
2018-09-10 22:25:56 -0400

    Added conditional return of all film fields except title and  link

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 let FilmCard = (props) => {
 
+    let producer = (
+        <div className="d-flex justify-content-between">
+            <p className="card-text">Produced by </p>
+            <p className="card-text">{props.prod}</p>
+        </div>
+    );
 
+    let director = (
+        <div className="d-flex justify-content-between">
+            <p className="card-text">Directed by </p>
+            <p className="card-text">{props.direc}</p>
+        </div>
+    );
 
+    let ratingAndDate = (
+        <div className="card-footer d-flex justify-content-between align-items-center">
+            Released: {props.date} <span>Rotten Tomato: <span className="badge badge-danger badge-pill text-center"> {props.rt}% </span></span>
+        </div>)
+
+    let description = <p className="card-text">{props.desc}</p>;
 
     return (
 
@@ -11,23 +29,16 @@ let FilmCard = (props) => {
             <div className="card-header text-center">
                 <h4> {props.title} </h4>
             </div>
-            <div className="card-footer d-flex justify-content-between align-items-center">
-                Released: {props.date} <span>Rotten Tomato: <span className="badge badge-danger badge-pill text-center"> {props.rt}% </span></span>
-            </div>
+            { props.date !== undefined && props.rt ? ratingAndDate : props.date}
             <div className="card-body">
-                <p className="card-text">{props.desc}</p>
+            {props.desc !== undefined ? description : props.desc}
                 <span className="d-flex justify-content-center"> {props.link}</span>
 
             </div>
             <div className="card-footer ">
-                <div className="d-flex justify-content-between">
-                    <p className="card-text">Directed by </p>
-                    <p className="card-text">{props.direc}</p>
-                </div>
-                <div className="d-flex justify-content-between">
-                    <p className="card-text">Produced by </p>
-                    <p className="card-text">{props.prod}</p>
-                </div>
+                {props.direc !== undefined ? director : props.direc}
+                {props.prod !== undefined ? producer : props.prod}
+
             </div>
 
         </ div>
