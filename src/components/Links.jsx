e21e4235c3f8e741e710e3e5f72364e74d0f8adc
lbarret4 <llewellynbar4@gmail.com>
2018-09-10 22:26:24 -0400

    moved links to center of page

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,7 +5,7 @@ import{ Link }from 'react-router-dom';
 const Links = () => {
 
     return (
-        <div className="btn-group mb-2" role="group">
+        <div className="btn-group mb-2 d-flex justify-content-center" role="group">
             <Link className="btn btn-dark" to="/">Go Home</Link>
             <Link className="btn btn-dark" to="/films">View Films</Link>
             <Link className="btn btn-dark" to="/people">View People</Link>
