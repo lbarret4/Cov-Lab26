e4795ee20f3d6bd6af8bea5b94e116e5cc1a871f
lbarret4 <llewellynbar4@gmail.com>
2018-09-11 00:54:49 -0400

    Complete task 4. Created routes for people

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,36 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Links from './Links';
-import Films from './Films';
+import Films from './Film/Films';
 import Home from './Home';
-import Film from './Film';
+import Film from './Film/Film';
+import People from './People/People';
+import Person from './People/Person';
 
 
 class App extends Component {
 
-        constructor(props){
-            super(props);
-            this.state ={
-                data: [],
-                cardtype: ""
-            }
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            cardtype: ""
         }
+
+    }
 
 
     render() {
         return (
             <Router>
-               <div className="container">
+                <div className="container">
                     <Links />
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route exact path="/films" component={Films} />
-                        <Route  path="/films/:id" component={Film} /> 
+                        <Route path="/films/:id" component={Film} />
+                        <Route exact path="/people" component={People} />
+                        <Route exact path="/people/:id" component={Person} />
                     </Switch>
                 </div>
             </Router>
